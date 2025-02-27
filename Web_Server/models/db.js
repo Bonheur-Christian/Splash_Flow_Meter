ea6b31c9cfbcd0cf3a7342bcd541bc8998a7b2bb
M.Bonheur <mbonheurc23@gmail.com>
2025-02-27 22:01:00 +0200

    some adjustments

--- Web_Server/models/db.js
+++ Web_Server/models/db.js
@@ -1,20 +1,19 @@
-const {Client} =require('pg');
+const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT
-
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 })
 
 
-client.connect(err=>{
-    if(err) throw err;
+client.connect(err => {
+    if (err) throw err;
 
-    console.log('Connected to database');  
+    console.log('Connected to database');
 })
 
 module.exports = client;
